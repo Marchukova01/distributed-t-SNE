1fd5f8a4c8ca4e8a882a0c273bdae6b503248344
Marchukova01 <90204625+Marchukova01@users.noreply.github.com>
2023-06-02 08:55:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ dim_low = 2 # embedding space dimension
 d = 4 # number of work nodes
 
 max_iter = 1000  # number of gradient descent steps
-n_steps = 100  # binary search
+n_steps = 100  # number of binary search steps for sigma value
 
 early_exaggeration = 12.0
 desired_perplexity = 30.0
 initial_momentum = 0.5
 final_momentum = 0.8
-eta = 400 # learning_rate
+eta = 50 # learning_rate
 min_gain = 0.01
 
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
@@ -25,8 +25,7 @@ X = np.array(train_X.reshape((k, m_coord)).T[:, 0:n_samples], dtype=float)
 y = np.array(train_y[0:n_samples])
 
 # the original vector in a lower dimension space
-Y  = np.random.rand(dim_low, n_samples)
-Y = Y / np.std(Y[0, :]) * 1e-4
+Y = 1e-4 * np.random.standard_normal(size=(dim_low, n_samples)).astype(np.float32)
 
-Y = tsne.tsne(X, Y, y, n_samples, m_coord, dim_low, d, max_iter, n_steps, early_exaggeration, desired_perplexity, eta)
+Y = tsne.tsne(X, Y, n_samples, m_coord, dim_low, d, max_iter, n_steps, early_exaggeration, desired_perplexity, eta)
 graph.graph_fun(Y, y)
